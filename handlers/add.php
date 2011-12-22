1592dcfb854dd41a54216dc41103bed1c49bc5d3
Johnny Broadway <johnny@johnnybroadway.com>
2011-12-22 13:20:39 -0600

    Added elasticsearch to add handler

--- handlers/add.php
+++ handlers/add.php
@@ -8,12 +8,7 @@ $failed = Form::verify_values ($this->data, 'apps/search/forms/add.php');
 if (count ($failed) > 0) {
 	die ('Validation error on fields: ' . join (', ', $failed));
 }
-
-require_once ('apps/search/lib/indextank_client.php');
-
-$client = new ApiClient ($appconf['IndexTank']['private_api_url']);
-$index = $client->get_index ($appconf['IndexTank']['index_name']);
-
+		
 $body = trim (strip_tags ($this->data['body']));
 $description = array_shift (explode ('.', $body));
 if (strlen ($description) > 160) {
@@ -22,12 +17,53 @@ if (strlen ($description) > 160) {
 $body = $this->data['page'] . ' ' . $this->data['title'] . $body;
 $url = '/' . $this->data['page'];
 
-// hopefully 200!
-return $index->add_document ($this->data['page'], array (
-	'title' => $this->data['title'],
-	'description' => $description,
-	'text' => $body,
-	'url' => $url
-));
+switch ($appconf['Search']['backend']) {
+	case 'indextank':
+		require_once ('apps/search/lib/indextank_client.php');
+		
+		$client = new ApiClient ($appconf['IndexTank']['private_api_url']);
+		$index = $client->get_index ($appconf['IndexTank']['index_name']);
+		
+		// hopefully 200!
+		return $index->add_document ($this->data['page'], array (
+			'title' => $this->data['title'],
+			'description' => $description,
+			'text' => $body,
+			'url' => $url
+		));
+
+	case 'elasticsearch':
+		require_once ('apps/search/lib/elastica_autoloader.php');
+
+		$servers = array ();
+		foreach ($appconf['ElasticSearch'] as $server) {
+			$servers[] = $server;
+		}
+
+		$client = new Elastica_Client (array ('servers' => $servers));
+		$index = $client->getIndex ('webpages');
+		$type = $index->getType ('webpage');
+
+		$doc = new Elastica_Document ($this->data['page'], array (
+			'id' => $this->data['page'],
+			'title' => $this->data['title'],
+			'description' => $description,
+			'text' => $body,
+			'url' => $url
+		));
+
+		$res = $type->addDocument ($doc);
+		if ($res->isOk ()) {
+			$index->refresh ();
+			return 200;
+		}
+		error_log ($res->getError ());
+		return 500;
+		break;
+
+	default:
+		error_log ('Unknown search backend: ' . $appconf['Search']['backend']);
+		return false;
+}
 
 ?>
