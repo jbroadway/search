8b6ef88209c4fb6ffd61b2b3ead150b77db717c7
Johnny Broadway <johnny@johnnybroadway.com>
2011-12-22 13:28:19 -0600

    Added initial search outline for elasticsearch

--- handlers/index.php
+++ handlers/index.php
@@ -1,30 +1,64 @@
 <?php
 
-$page->add_script ('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/jquery-ui.min.js"></script>
-<script type="text/javascript" src="/apps/search/js/indextank/jquery.indextank.ize.js"></script>
-<script type="text/javascript" src="/apps/search/js/indextank/jquery.indextank.autocomplete.js"></script>
-<script type="text/javascript" src="/apps/search/js/indextank/jquery.indextank.ajaxsearch.js"></script>
-<script type="text/javascript" src="/apps/search/js/indextank/jquery.indextank.renderer.js"></script>
-<script type="text/javascript" src="/apps/search/js/indextank/jquery.indextank.instantsearch.js"></script>
-<script type="text/javascript" src="/apps/search/js/indextank/jquery.indextank.basic.js"></script>
-<link type="text/css" rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/themes/' . $appconf['jQuery']['ui_theme'] . '/jquery-ui.css" media="all" />
-<style type="text/css">
-.result {
-	padding-bottom: 10px;
-}
-.result a {
-	font-weight: bold;
-	display: block;
-}
-</style>
-<script>
-$(document).ready(function(){
-	$("#search-form").indextank_Ize(\'' . $appconf['IndexTank']['public_api_url'] . '\', \'' . $appconf['IndexTank']['index_name'] . '\');
-	var renderer =  $("#search-results").indextank_Renderer();
-	$("#search-query").indextank_Autocomplete().indextank_AjaxSearch( {listeners: renderer}).indextank_InstantSearch();
-});
-</script>');
+switch ($appconf['Search']['backend']) {
+	case 'indextank':
+		$page->add_script ('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/jquery-ui.min.js"></script>
+		<script type="text/javascript" src="/apps/search/js/indextank/jquery.indextank.ize.js"></script>
+		<script type="text/javascript" src="/apps/search/js/indextank/jquery.indextank.autocomplete.js"></script>
+		<script type="text/javascript" src="/apps/search/js/indextank/jquery.indextank.ajaxsearch.js"></script>
+		<script type="text/javascript" src="/apps/search/js/indextank/jquery.indextank.renderer.js"></script>
+		<script type="text/javascript" src="/apps/search/js/indextank/jquery.indextank.instantsearch.js"></script>
+		<script type="text/javascript" src="/apps/search/js/indextank/jquery.indextank.basic.js"></script>
+		<link type="text/css" rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/themes/' . $appconf['jQuery']['ui_theme'] . '/jquery-ui.css" media="all" />
+		<style type="text/css">
+		.result {
+			padding-bottom: 10px;
+		}
+		.result a {
+			font-weight: bold;
+			display: block;
+		}
+		</style>
+		<script>
+		$(document).ready(function(){
+			$("#search-form").indextank_Ize(\'' . $appconf['IndexTank']['public_api_url'] . '\', \'' . $appconf['IndexTank']['index_name'] . '\');
+			var renderer =  $("#search-results").indextank_Renderer();
+			$("#search-query").indextank_Autocomplete().indextank_AjaxSearch( {listeners: renderer}).indextank_InstantSearch();
+		});
+		</script>');
+		
+		echo $tpl->render ('search/index');
+		return;
 
-echo $tpl->render ('search/index');
+	case 'elasticsearch':
+		if ($_GET['query']) {
+			require_once ('apps/search/lib/elastica_autoloader.php');
+	
+			$servers = array ();
+			foreach ($appconf['ElasticSearch'] as $server) {
+				$servers[] = $server;
+			}
+	
+			$client = new Elastica_Client (array ('servers' => $servers));
+			$index = $client->getIndex ('webpages');
+			$type = $index->getType ('webpage');
+
+			$query = new Elastica_Query_Terms ();
+			$query->setTerms ('title' => explode (' ', trim ($_GET['query'])));
+			$res = $type->search ($query);
+
+			$total = $res->count ();
+
+			echo $tpl->render ('search/results', array (
+				'results' => $res,
+				'total' => $total,
+				'query' => $_GET['query']
+			));
+		} else {
+			echo $tpl->render ('search/index');
+		}
+		return;
+
+}
 
 ?>
