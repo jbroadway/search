9f3963acb754304d04b30510e1f96e92f1dde1d3
Johnny Broadway <johnny@johnnybroadway.com>
2011-12-22 12:01:53 -0600

    Added elasticsearch to config

--- conf/config.php
+++ conf/config.php
@@ -1,6 +1,28 @@
 ; <?php /*
 
+[Search]
+
+; To choose a back-end search engine, uncomment one of the following
+; options:
+
+backend = elasticsearch
+;backend = indextank
+
+[ElasticSearch]
+
+; ElasticSearch server configuration goes here:
+
+server1[host] = localhost
+server1[port] = 9200
+
+; To add additional servers, use:
+
+;server2[host] = localhost
+;server2[port] = 9201
+
 [IndexTank]
+
+; IndexTank server configuration goes here:
 
 public_api_url = "http://*****.api.indextank.com/"
 
