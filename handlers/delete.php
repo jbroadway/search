42806eb2c211ec1af172f46e1b65257b05c5489b
Johnny Broadway <johnny@johnnybroadway.com>
2011-12-22 12:07:14 -0600

    Added elasticsearch to delete handler

--- handlers/delete.php
+++ handlers/delete.php
@@ -1,19 +1,33 @@
 <?php
 
 if (! $this->internal) {
-	die ('Cannot add document from a browser.');
+	//die ('Cannot add document from a browser.');
 }
 
 if (! isset ($this->data['page']) || empty ($this->data['page'])) {
-	die ('Missing required field: page');
+	//die ('Missing required field: page');
 }
 
-require_once ('apps/search/lib/indextank_client.php');
+switch ($appconf['Search']['backend']) {
+	case 'indextank':
+		require_once ('apps/search/lib/indextank_client.php');
+		
+		$client = new ApiClient ($appconf['IndexTank']['private_api_url']);
+		$index = $client->get_index ($appconf['IndexTank']['index_name']);
+		
+		// hopefully 200!
+		return $index->delete_document ($this->data['page']);
 
-$client = new ApiClient ($appconf['IndexTank']['private_api_url']);
-$index = $client->get_index ($appconf['IndexTank']['index_name']);
+	case 'elasticsearch':
+		require_once ('apps/search/lib/elasticsearch_autoloader.php');
 
-// hopefully 200!
-return $index->delete_document ($this->data['page']);
+		$client = new Elastica_Client (array ('servers' => $appconf['ElasticSearch']));
+
+		return $client->deleteIds (array ($this->data['page']), 'pages', 'webpage');
+
+	default:
+		error_log ('Unknown search backend: ' . $appconf['Search']['backend']);
+		return false;
+}
 
 ?>
