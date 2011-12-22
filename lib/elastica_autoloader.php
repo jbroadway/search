7eb2cc7b29c028592c10f096492c484c7b2e199e
Johnny Broadway <johnny@johnnybroadway.com>
2011-12-22 12:06:52 -0600

    Added elastica autoloader

---
+++ lib/elastica_autoloader.php
@@ -0,0 +1,13 @@
+<?php
+
+function __autoload_elastica ($class) {
+	$path = str_replace ('_', '/', $class);
+
+    if (file_exists ('apps/search/lib/Elastica/lib/' . $path . '.php')) {
+        require_once ('apps/search/lib/Elastica/lib/' . $path . '.php');
+    }
+}
+
+spl_autoload_register ('__autoload_elastica', true, true);
+
+?>
